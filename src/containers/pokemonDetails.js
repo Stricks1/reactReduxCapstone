6d4d1916834cc25dfc7de7b2594171174ce136c9
Stricks1 <gmalheiross@gmail.com>
2020-10-20 11:54:59 -0300

    Fix: fixed fetch data to clean info just when calling API for gather new data

--- src/containers/pokemonDetails.js
+++ src/containers/pokemonDetails.js
@@ -52,8 +52,6 @@ class PokemonsDetails extends Component {
     const {
       ChangeDetail, ChangeLoading, pokemons, ChangeMessage,
     } = this.props;
-    ChangeDetail({});
-    ChangeLoading(true);
     if (pokemons.length === 0) {
       return;
     }
@@ -61,6 +59,8 @@ class PokemonsDetails extends Component {
     const url2 = 'https://pokeapi.co/api/v2/pokemon-species/';
     axios.get(url + number)
       .then(data => {
+        ChangeDetail({});
+        ChangeLoading(true);
         axios.get(url2 + number)
           .then(data2 => {
             let chainUrl = 'unknown';
