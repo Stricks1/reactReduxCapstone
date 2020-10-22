72e2c831db7ab9f229063a10953c02682ae36596
Stricks1 <gmalheiross@gmail.com>
2020-10-22 12:06:08 -0300

    Changes on where loading

--- src/containers/pokemonDetails.js
+++ src/containers/pokemonDetails.js
@@ -55,6 +55,8 @@ class PokemonsDetails extends Component {
     const {
       ChangeDetail, ChangeLoading, pokemons, ChangeMessage,
     } = this.props;
+    ChangeDetail({});
+    ChangeLoading(true);
     if (pokemons.length === 0) {
       return;
     }
@@ -62,8 +64,6 @@ class PokemonsDetails extends Component {
     const url2 = 'https://pokeapi.co/api/v2/pokemon-species/';
     axios.get(url + number)
       .then(data => {
-        ChangeDetail({});
-        ChangeLoading(true);
         axios.get(url2 + number)
           .then(data2 => {
             let chainUrl = 'unknown';
